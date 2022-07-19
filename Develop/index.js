74b6aa4c3003241ebb38caa32378f098820ab82b
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-07-19 21:53:19 +1000

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -11,3 +11,34 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+const inquirerPrompt = function() {
+    return inquirer
+    .prompt([
+    {
+      type: 'input',
+      message: 'What is your  name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'What is your location?',
+      name: 'location',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your bio: ',
+      name: 'bio',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your LinkedIn URL: ',
+      name: 'linkedIn',
+    },
+    {
+      type: 'input',
+      message: 'Please enter your GitHub URL: ',
+      name: 'github',
+    },
+  ])
+}
