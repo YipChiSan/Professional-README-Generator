7bbe7157b91a080e0606b2583c6b40fd646f768d
YipChiSan <57831867+YipChiSan@users.noreply.github.com>
2022-07-20 10:48:58 +1000

    Write down the questions that I am going to ask to the user

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'userName',
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'What is your project description?',
+    name: 'projectDesc',
+  },
+  {
+    type: 'input',
+    message: 'What is your installation guide? ',
+    name: 'installation',
+  },
+  {
+    type: 'input',
+    message: 'What is the usage of your app?',
+    name: 'usage',
+  },
+  {
+    type: 'list',
+    message: "What is the license of this project?",
+    choices: ['Apache License 2.0', 'MIT license', 'GNU General Public License (GPL)'],
+    name: 'license',
+  },
+  {
+    type: 'input',
+    message: 'What is the contribution guideline?',
+    name: 'contributing',
+  },
+  {
+    type: 'input',
+    message: 'Any tests you have done on this project?',
+    name: 'test',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -14,31 +63,5 @@ init();
 
 const inquirerPrompt = function() {
     return inquirer
-    .prompt([
-    {
-      type: 'input',
-      message: 'What is your  name?',
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: 'What is your location?',
-      name: 'location',
-    },
-    {
-      type: 'input',
-      message: 'Please enter your bio: ',
-      name: 'bio',
-    },
-    {
-      type: 'input',
-      message: 'Please enter your LinkedIn URL: ',
-      name: 'linkedIn',
-    },
-    {
-      type: 'input',
-      message: 'Please enter your GitHub URL: ',
-      name: 'github',
-    },
-  ])
+    .prompt(questions)
 }
